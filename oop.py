81bf3bd315fb1d9598c1147454fa036b653c70f7
adilkaaa <adilkhanakakov@gmail.com>
2022-04-19 20:35:26 +0600

    add Calculator

---
+++ oop.py
@@ -0,0 +1,56 @@
+class Calculator:
+    def __init__(self,num1=0,num2=0,sign="+"):
+        self.__num1 = num1
+        self.__num2 = num2
+        self.__sign = sign
+        self.answer = self.__calculate()
+        
+
+    def __call__(self, *args, **kwds):
+        return self.answer
+
+    def __calculate(self):
+        r = eval(f"{self.__num1}{self.__sign}{self.__num2}")# 5+9
+        return r
+          
+
+    def __add__(self,other):
+        return self.__calculate() + other
+    
+    def __sub__(self,other):
+        return self.__calculate() - other
+
+    def __mul__(self,other):
+        return self.__calculate() * other
+
+    def __truediv__(self,other):
+        return self.__calculate()/other
+
+    def __mod__(self,other):
+        return self.__calculate()%other
+    
+    def __floordiv__(self,other):
+        return self.__calculate()//other
+    
+   
+  
+a = Calculator(6,3,"/")
+a1 = Calculator(63,3,"/")
+
+print(a())
+print(a1())
+b = a()+a1()
+# a+=a1()
+a = a+a1()
+print(a)
+
+
+
+
+
+
+
+
+
+
+
